8a31bb37dc50e19d070ef06fbda61495fccf74af
twall <tomwall.col@gmail.com>
2017-01-27 19:33:38 -0800

    initial commit of react stuff

---
+++ react-boilerplate/app/app.jsx
@@ -0,0 +1,8 @@
+// we removed the script tags from the html, so now need to require
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h3>React Boilerplate Working!!</h3>,
+document.getElementById('app')
+);
