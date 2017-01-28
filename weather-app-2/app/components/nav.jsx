9f2ba4f126f39a45238bbc58070a1334cbae6c44
twall <tomwall.col@gmail.com>
2017-01-27 20:29:37 -0800

    added foundation classes for nav

--- weather-app-2/app/components/nav.jsx
+++ weather-app-2/app/components/nav.jsx
@@ -4,16 +4,33 @@ var {Link, IndexLink} = require('react-router');
 // var Link = require('react-router').Link
 
 var Nav = React.createClass({
+  onSearch(e) {
+    e.preventDefault();
+  },
   render() {
     return (
-      <div>
-        <ul className="nav nav-tabs">
-          {/* Link generates anchor tag*/}
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather</li>
+            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+            <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+            <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
 
-          <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
-          <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
-          <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
-        </ul>
       </div>
     );
   }
